8f0b448334ff9478e3bbfa693ab40f6abd93d77d
Elisha OYAWALE <temmyscope@Elishas-MacBook-Air.local>
2021-10-05 22:26:49 +0100

    add models

--- app/Http/Livewire/Admin/AddProduct.php
+++ app/Http/Livewire/Admin/AddProduct.php
@@ -6,10 +6,17 @@ use Livewire\Component;
 
 class AddProduct extends Component
 {
+    public $name;
+    public $description;
+ 
+    protected $rules = [
+        'name' => 'required|min:6',
+        'description' => 'required',
+    ];
 
     public function create()
     {
-        
+        $this->validate();
     }
 
     public function render()
