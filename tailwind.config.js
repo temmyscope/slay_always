1e9ea9d557f1df0dc193173382c21933d5e4fd56
samfrexz <samueloyawale315@gmail.com>
2021-10-09 18:29:34 +0100

    added/video-bg

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,10 @@ module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
+    height: {
+      vid: '65vh',
+      smVid: '45vh'
+    },
     backgroundColor: theme => ({
       ...theme('colors'),
       bgPry: '#010101'
@@ -16,7 +20,19 @@ module.exports = {
         inptW: '25%',
         iconW: '35%',
         slid: '50%'
-      }
+      },
+      fontFamily: {
+        slayFont: ['Nunito']
+      },
+      // transform: {
+      //   cad: 'translate(-50%, -50%)'
+      // }
+      // backgroundImage: {
+      //   cad: "url(/public/assets/footies1.PNG)",
+      //   cad2: "url(/public/assets/cloth2.PNG)",
+      //   cad3: "url(/public/assets/cloth1.PNG)",
+      //   cad4: "url(/public/assets/cloth3.PNG)",
+      // }
     },
   },
   variants: {
