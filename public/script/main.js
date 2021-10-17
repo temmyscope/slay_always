7a1e17702789fc719e28f79f8abd313fd82d4db5
samfrexz <samueloyawale315@gmail.com>
2021-10-17 19:02:32 +0100

    added/footer

--- public/script/main.js
+++ public/script/main.js
@@ -105,3 +105,50 @@ for (i = 0; i < acc.length; i++) {
     } 
   });
 }
+
+
+// var slide = 1;
+// showImageSlides(slide);
+
+// function plusSlides(n) {
+//   showImageSlides(slide += n);
+// }
+
+// function currentSlide(n) {
+//   showImageSlides(slide = n);
+// }
+
+// function showImageSlides(n) {
+//   var i;
+//   var slides = document.getElementsByClassName("mySlidex");
+//   var dots = document.getElementsByClassName("dot");
+//   if (n > slides.length) {slide = 1}    
+//   if (n < 1) {slide = slides.length}
+//   for (i = 0; i < slides.length; i++) {
+//       slides[i].style.display = "none";  
+//   }
+//   for (i = 0; i < dots.length; i++) {
+//       dots[i].className = dots[i].className.replace(" active", "");
+//   }
+//   slides[slide-1].style.display = "block";  
+//   dots[slide-1].className += " active";
+// }
+
+// const radio = document.querySelector('#slide1')
+
+// if (radio.checked) {
+  
+// }
+
+// footer accordion
+const accordion = document.querySelectorAll(".footer-accordion");
+var i;
+
+for (i = 0; i < accordion.length; i++) {
+  accordion[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling; 
+    panel.style.maxHeight ? panel.style.maxHeight = null : panel.style.maxHeight = panel.scrollHeight + "px";
+  });
+}
+
